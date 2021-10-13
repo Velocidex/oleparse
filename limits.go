0334d69593c1223a0dbaf66efd59c4dd6d4a4d38
Mike Cohen <scudette@users.noreply.github.com>
2021-10-13 16:39:43 +1000

    Added limit checks to improve stability (#4)

---
+++ limits.go
@@ -0,0 +1,8 @@
+package oleparse
+
+const (
+	// Limit the size of the document
+	MAX_SECTORS = 1024 * 1024
+
+	MAX_SECTOR_SHIFT = 10
+)
--- utils.go
+++ utils.go
@@ -5,3 +5,24 @@ import "github.com/davecgh/go-spew/spew"
 func Debug(arg interface{}) {
 	spew.Dump(arg)
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func uint32_min(a, b uint32) uint32 {
+	if a < b {
+		return a
+	}
+	return b
+}
