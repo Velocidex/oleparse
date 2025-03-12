c553efa5a498f3a11fab656bc52ec13cd777faf1
Max Altgelt <max.altgelt@nextron-systems.com>
2025-03-12 11:11:47 +0100

    fix: detect issues by fuzzing
    Create two fuzz tests and fix some panics / loops shown by the
    fuzzer.

--- oleparse_test.go
+++ oleparse_test.go
@@ -1,7 +1,10 @@
 package oleparse
 
 import (
+	"archive/zip"
 	"encoding/json"
+	"io"
+	"strings"
 	"testing"
 
 	"github.com/sebdah/goldie"
@@ -15,4 +18,73 @@ func TestMacros(t *testing.T) {
 
 	serialized, _ := json.MarshalIndent(macros, " ", " ")
 	goldie.Assert(t, "vba_macros", serialized)
+}
+
+func FuzzExtractMacros(f *testing.F) {
+	r, err := zip.OpenReader("test_data/xlswithmacro.xlsm")
+	if err != nil {
+		f.Fatal(err)
+	}
+	defer r.Close()
+
+	for _, file := range r.File {
+		if BINFILE_NAME.MatchString(file.Name) {
+			rc, err := file.Open()
+			if err != nil {
+				f.Fatal(err)
+			}
+			data, err := io.ReadAll(rc)
+			if err != nil {
+				f.Fatal(err)
+			}
+			f.Add(data)
+		}
+	}
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		modules, err := ParseBuffer(data)
+		if err != nil {
+			t.Skip()
+		}
+		var macros strings.Builder
+		for _, module := range modules {
+			macros.WriteString(module.Code)
+		}
+		t.Log(macros.String())
+	})
+}
+
+func FuzzDecompressStream(f *testing.F) {
+	r, err := zip.OpenReader("test_data/xlswithmacro.xlsm")
+	if err != nil {
+		f.Fatal(err)
+	}
+	defer r.Close()
+
+	for _, file := range r.File {
+		if BINFILE_NAME.MatchString(file.Name) {
+			rc, err := file.Open()
+			if err != nil {
+				f.Fatal(err)
+			}
+			data, err := io.ReadAll(rc)
+			if err != nil {
+				f.Fatal(err)
+			}
+
+			oleFile, err := NewOLEFile(data)
+			if err != nil {
+				f.Fatal(err)
+			}
+
+			for _, dir := range oleFile.Directory {
+				f.Add(dir.data)
+			}
+		}
+	}
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		decompressed := DecompressStream(data)
+		t.Log(decompressed)
+	})
 }
